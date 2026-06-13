48ba6a4e7f84943dae58760dbd39f5c3b3be2b55
t90Freelancer <137405210+t90Freelancer@users.noreply.github.com>
2026-06-13 17:34:39 +0700

    up

--- src/app.js
+++
@@ -1,9 +0,0 @@
-const onxrloaded = () => {
-  XR8.XrController.configure({
-    imageTargetData: [
-      require('../image-targets/bia-hanoi-premium.json'),
-    ],
-  })
-  XR8.addCameraPipelineModule(LandingPage.pipelineModule())
-}
-window.XR8 ? onxrloaded() : window.addEventListener('xrloaded', onxrloaded)
